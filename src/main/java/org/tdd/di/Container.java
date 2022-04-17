3eaed211a21f1acd18e173d2dddd7069e481d47e
xudong.ren <xudong.ren@thoughtworks.com>
2022-04-17 15:02:49 +0800

    [refactor] readable

--- src/main/java/org/tdd/di/Container.java
+++ src/main/java/org/tdd/di/Container.java
@@ -1,31 +1,34 @@
 package org.tdd.di;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Container {
-    private static final Map<Class<?>, Object> INSTANCE_MAP = new HashMap<>();
 
+    private static final Map<Class<?>, Object> INSTANCE_MAP = new HashMap<>();
     private static final Map<Class<?>, Class<?>> TYPE_BIND_MAP = new HashMap<>();
 
     public <Type> void bind(Class<Type> type, Type instance) {
         INSTANCE_MAP.put(type, instance);
     }
 
+    public <Type> void bind(Class<Type> type, Class<? extends Type>  implType) {
+        TYPE_BIND_MAP.put(type, implType);
+    }
+
     public <Type> Type get(Class<Type> type) {
         Object instance = INSTANCE_MAP.get(type);
         if (instance == null) {
             try {
-                return (Type) TYPE_BIND_MAP.get(type).getDeclaredConstructors()[0].newInstance();
+                Class<?> implType = TYPE_BIND_MAP.get(type);
+                Constructor<?> defaultConstructor = implType.getDeclaredConstructors()[0];
+                instance = defaultConstructor.newInstance();
             } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
                 new RuntimeException(e);
             }
         }
         return (Type) instance;
-    }
-
-    public <Type> void bind(Class<Type> type, Class<? extends Type>  subType) {
-        TYPE_BIND_MAP.put(type, subType);
     }
 }
