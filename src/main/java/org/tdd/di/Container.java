34de5c7a8191e63ac86036eaaa395871425a8321
xudong.ren <xudong.ren@thoughtworks.com>
2022-04-17 15:45:03 +0800

    [main] pass 3rd test 有依赖的组件，通过 Inject 标注的构造函数生成组件实例

--- src/main/java/org/tdd/di/Container.java
+++ src/main/java/org/tdd/di/Container.java
@@ -1,9 +1,13 @@
 package org.tdd.di;
 
+import jakarta.inject.Inject;
+
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class Container {
 
@@ -14,21 +18,32 @@ public class Container {
         INSTANCE_MAP.put(type, instance);
     }
 
-    public <Type> void bind(Class<Type> type, Class<? extends Type>  implType) {
+    public <Type> void bind(Class<Type> type, Class<? extends Type> implType) {
         TYPE_BIND_MAP.put(type, implType);
     }
 
     public <Type> Type get(Class<Type> type) {
-        Object instance = INSTANCE_MAP.get(type);
+        Type instance = (Type) INSTANCE_MAP.get(type);
         if (instance == null) {
-            try {
-                Class<?> implType = TYPE_BIND_MAP.get(type);
-                Constructor<?> defaultConstructor = implType.getDeclaredConstructors()[0];
-                instance = defaultConstructor.newInstance();
-            } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
-                new RuntimeException(e);
-            }
+            Class<?> implType = TYPE_BIND_MAP.get(type);
+
+            Constructor<Type>[] constructors = (Constructor<Type>[]) implType.getDeclaredConstructors();
+            instance = Arrays.stream(constructors).filter(c -> Objects.nonNull(c.getAnnotation(Inject.class))).findFirst().map(c -> {
+                try {
+                    return c.newInstance(Arrays.stream(c.getParameterTypes()).map(p -> get(p)).toArray());
+                } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+                    new RuntimeException(e);
+                }
+                return null;
+            }).orElseGet(() -> {
+                try {
+                    return constructors[0].newInstance();
+                } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+                    new RuntimeException(e);
+                }
+                return null;
+            });
         }
-        return (Type) instance;
+        return instance;
     }
 }
