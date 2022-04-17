e0963e7e8ba42d25256f272ac1edaab69cafaf49
xudong.ren <xudong.ren@thoughtworks.com>
2022-04-17 17:51:10 +0800

    [task] finished 3rd task and refine test
    有依赖的组件，通过 Inject 标注的构造函数生成组件实例。

--- src/test/java/org/tdd/di/ContainerTest.java
+++ src/test/java/org/tdd/di/ContainerTest.java
@@ -1,6 +1,7 @@
 package org.tdd.di;
 
 import jakarta.inject.Inject;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
@@ -8,12 +9,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ContainerTest {
 
+    private Container container;
+
+    @BeforeEach
+    void setUp() {
+        container = new Container();
+    }
+
     @Nested
     class ComponentConstruction {
 
         @Test
         void should_return_the_component_when_get_if_the_component_bind() {
-            Container container = new Container();
             Component componentImpl = new Component() {
             };
             container.bind(Component.class, componentImpl);
@@ -23,7 +30,6 @@ public class ContainerTest {
 
         @Test
         void should_return_a_component_when_get_if_the_bind_component_no_dependency() {
-            Container container = new Container();
             container.bind(Component.class, ComponentNoDependency.class);
 
             assertNotNull(container.get(Component.class));
@@ -32,7 +38,6 @@ public class ContainerTest {
 
         @Test
         void should_return_a_correct_component_when_get_if_the_bind_component_has_dependency_inject_by_constructor() {
-            Container container = new Container();
             container.bind(AnotherComponent.class, ComponentWithDependency.class);
             container.bind(Component.class, ComponentNoDependency.class);
 
