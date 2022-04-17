67e8ef81b7e37b519e0fc1ecfd8159379fe4adc4
xudong.ren <xudong.ren@thoughtworks.com>
2022-04-17 17:23:51 +0800

    [refactor] replace instance map

--- src/main/java/org/tdd/di/Container.java
+++ src/main/java/org/tdd/di/Container.java
@@ -5,14 +5,15 @@ import jakarta.inject.Inject;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.*;
+import java.util.function.Supplier;
 
 public class Container {
 
-    private static final Map<Class<?>, Object> INSTANCE_MAP = new HashMap<>();
     private static final Map<Class<?>, Class<?>> TYPE_BIND_MAP = new HashMap<>();
+    private static final Map<Class<?>, Supplier<?>> MAP = new HashMap<>();
 
     public <Type> void bind(Class<Type> type, Type instance) {
-        INSTANCE_MAP.put(type, instance);
+        MAP.put(type, () -> instance);
     }
 
     public <Type> void bind(Class<Type> type, Class<? extends Type> implType) {
@@ -20,10 +21,8 @@ public class Container {
     }
 
     public <Type> Type get(Class<Type> type) {
-        Class<?> implType = TYPE_BIND_MAP.get(type);
-        Object instance = INSTANCE_MAP.get(type);
-        return Optional.ofNullable((Type) instance).orElseGet(() -> {
-            Constructor<Type>[] constructors = (Constructor<Type>[]) implType.getDeclaredConstructors();
+        return Optional.ofNullable(MAP.get(type)).map(p -> (Type) p.get()).orElseGet(() -> {
+            Constructor<Type>[] constructors = (Constructor<Type>[]) TYPE_BIND_MAP.get(type).getDeclaredConstructors();
             return Arrays.stream(constructors).filter(c -> Objects.nonNull(c.getAnnotation(Inject.class)))
                     .findFirst().map(this::newInstanceWith).orElseGet(() -> newInstanceWith(constructors[0]));
         });
