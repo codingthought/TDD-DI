3df047176b4245b1fe17ddc366d7c41b704cc2c6
xudong.ren <xudong.ren@thoughtworks.com>
2022-05-01 17:26:03 +0800

    [test] rename test

--- src/test/java/org/tdd/di/ContainerTest.java
+++ src/test/java/org/tdd/di/ContainerTest.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class ContainerBuilderTest {
+public class ContainerTest {
 
     private ContainerBuilder containerBuilder;
 
