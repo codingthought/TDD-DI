b46f470101c95664c3a829e488289ae2347f10f2
xudong.ren <xudong.ren@thoughtworks.com>
2022-05-01 22:20:38 +0800

    [refactor] move getConstructor

--- src/main/java/org/tdd/di/ContainerBuilder.java
+++ src/main/java/org/tdd/di/ContainerBuilder.java
@@ -1,11 +1,8 @@
 package org.tdd.di;
 
-import jakarta.inject.Inject;
 import org.tdd.di.exception.CycleDependencyNotAllowed;
 import org.tdd.di.exception.DependencyNotFoundException;
-import org.tdd.di.exception.IllegalComponentException;
 
-import java.lang.reflect.Constructor;
 import java.util.*;
 
 public class ContainerBuilder {
@@ -18,29 +15,8 @@ public class ContainerBuilder {
     }
 
     public <Type> ContainerBuilder bind(Class<Type> type, Class<? extends Type> implType) {
-        Constructor<?> constructor = getConstructor(implType);
-        componentProviders.put(type, new InjectConstructionProvider<>(constructor));
+        componentProviders.put(type, new InjectConstructionProvider<>(InjectConstructionProvider.getConstructor(implType)));
         return this;
-    }
-
-    private static <Type> Constructor<?> getConstructor(Class<? extends Type> implType) {
-        Constructor<?> constructor;
-        Constructor<?>[] declaredConstructors = implType.getDeclaredConstructors();
-        List<Constructor<?>> filteredConstructors = Arrays.stream(declaredConstructors)
-                .filter(c -> Objects.nonNull(c.getAnnotation(Inject.class))).toList();
-        if (filteredConstructors.size() > 1) {
-            throw new IllegalComponentException();
-        }
-        if (filteredConstructors.size() == 0 &&
-                Arrays.stream(declaredConstructors).allMatch(d -> d.getParameterTypes().length > 0)) {
-            throw new IllegalComponentException();
-        }
-        if (filteredConstructors.size() == 1) {
-            constructor = filteredConstructors.get(0);
-        } else {
-            constructor = declaredConstructors[0];
-        }
-        return constructor;
     }
 
     public Container build() {
--- src/main/java/org/tdd/di/InjectConstructionProvider.java
+++ src/main/java/org/tdd/di/InjectConstructionProvider.java
@@ -1,11 +1,35 @@
 package org.tdd.di;
 
+import jakarta.inject.Inject;
+import org.tdd.di.exception.IllegalComponentException;
+
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 record InjectConstructionProvider<Type>(Constructor<Type> constructor) implements ComponentProvider<Type> {
+
+    static <Type> Constructor<?> getConstructor(Class<? extends Type> implType) {
+        Constructor<?> constructor;
+        Constructor<?>[] declaredConstructors = implType.getDeclaredConstructors();
+        List<Constructor<?>> filteredConstructors = Arrays.stream(declaredConstructors)
+                .filter(c -> Objects.nonNull(c.getAnnotation(Inject.class))).toList();
+        if (filteredConstructors.size() > 1) {
+            throw new IllegalComponentException();
+        }
+        if (filteredConstructors.size() == 0 &&
+                Arrays.stream(declaredConstructors).allMatch(d -> d.getParameterTypes().length > 0)) {
+            throw new IllegalComponentException();
+        }
+        if (filteredConstructors.size() == 1) {
+            constructor = filteredConstructors.get(0);
+        } else {
+            constructor = declaredConstructors[0];
+        }
+        return constructor;
+    }
 
     @Override
     public Type getFrom(Container container) {
