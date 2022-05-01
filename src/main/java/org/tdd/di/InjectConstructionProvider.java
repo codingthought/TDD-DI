e4e5c181322ef60a425cdddf0d4b10318925d7e6
xudong.ren <xudong.ren@thoughtworks.com>
2022-05-01 22:08:17 +0800

    [refactor] refine InjectConstructionProvider

--- src/main/java/org/tdd/di/InjectConstructionProvider.java
+++ src/main/java/org/tdd/di/InjectConstructionProvider.java
@@ -1,40 +1,21 @@
 package org.tdd.di;
 
-import org.tdd.di.exception.CycleDependencyNotAllowed;
-import org.tdd.di.exception.DependencyNotFoundException;
-
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 import java.util.List;
 
-class InjectConstructionProvider<Type> implements ComponentProvider<Type> {
-    private final Constructor<Type> constructor;
-    private boolean constructing = false;
-
-    public InjectConstructionProvider(Constructor<Type> constructor) {
-        this.constructor = constructor;
-    }
+record InjectConstructionProvider<Type>(Constructor<Type> constructor) implements ComponentProvider<Type> {
 
     @Override
     public Type getFrom(Container container) {
-        if (constructing) {
-            throw new CycleDependencyNotAllowed(constructor.getDeclaringClass());
-        }
         try {
-            constructing = true;
             return constructor.newInstance(Arrays.stream(constructor.getParameterTypes())
-                    .map(p -> container.get(p).orElseThrow(() ->
-                            new DependencyNotFoundException(constructor.getDeclaringClass(), p)))
+                    .map(p -> container.get(p).get())
                     .toArray());
-        } catch (CycleDependencyNotAllowed e) {
-            throw new CycleDependencyNotAllowed(e, constructor.getDeclaringClass());
         } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
-            new RuntimeException(e);
-        } finally {
-            constructing = false;
+            throw new RuntimeException(e);
         }
-        return null;
     }
 
     @Override
