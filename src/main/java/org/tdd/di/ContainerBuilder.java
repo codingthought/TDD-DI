70707d6aa4e52887a901bb25f1a015b9adbe98ea
xudong.ren <xudong.ren@thoughtworks.com>
2022-05-04 20:53:16 +0800

    [refactor] refine check dependencies

--- src/main/java/org/tdd/di/ContainerBuilder.java
+++ src/main/java/org/tdd/di/ContainerBuilder.java
@@ -13,43 +13,44 @@ public class ContainerBuilder {
 
     private final Map<Class<?>, ComponentProvider<?>> componentProviders = new HashMap<>();
 
-    public <Type> ContainerBuilder bind(Class<Type> type, Type instance) {
+    public <T> ContainerBuilder bind(Class<T> type, T instance) {
         componentProviders.put(type, (container) -> instance);
         return this;
     }
 
-    public <Type> ContainerBuilder bind(Class<Type> type, Class<? extends Type> implType) {
+    public <T> ContainerBuilder bind(Class<T> type, Class<? extends T> implType) {
         componentProviders.put(type, new InjectComponentProvider<>(implType));
         return this;
     }
 
     public Container build() {
-        componentProviders.keySet().forEach(component -> checkCycleDependency(component, new Stack<>()));
+        componentProviders.keySet().forEach(component -> checkDependencies(component, new Stack<>()));
         return new Container(componentProviders);
     }
 
-    private void checkCycleDependency(Class<?> component, Stack<Class<?>> stack) {
+    private void checkDependencies(Class<?> component, Stack<Class<?>> stack) {
         for (Type dependency : componentProviders.get(component).getTypeDependencies()) {
             if (dependency instanceof Class<?>) {
-                checkDependency(component, stack, (Class<?>) dependency);
+                checkExist(component, (Class<?>) dependency);
+                checkCycleDependencies(stack, (Class<?>) dependency);
             } else {
-                Class<?> type = (Class<?>)((ParameterizedType)dependency).getActualTypeArguments()[0];
-                if (!componentProviders.containsKey(type)) {
-                    throw new DependencyNotFoundException(component, type);
-                }
+                checkExist(component, (Class<?>)((ParameterizedType)dependency).getActualTypeArguments()[0]);
             }
         }
     }
 
-    private void checkDependency(Class<?> component, Stack<Class<?>> stack, Class<?> dependency) {
+    private void checkExist(Class<?> component, Class<?> dependency) {
         if (!componentProviders.containsKey(dependency)) {
             throw new DependencyNotFoundException(component, dependency);
         }
+    }
+
+    private void checkCycleDependencies(Stack<Class<?>> stack, Class<?> dependency) {
         if (stack.contains(dependency)) {
             throw new CycleDependencyNotAllowed(stack);
         }
         stack.push(dependency);
-        checkCycleDependency(dependency, stack);
+        checkDependencies(dependency, stack);
         stack.pop();
     }
 
